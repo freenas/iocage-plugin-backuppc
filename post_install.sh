7de4e142830efc2ac5ab9593eaf87bb7217cdfc7
Jenkins Node <jenkins@example.com>
2018-11-04 13:13:12 +0000

    - Fix permission

--- post_install.sh
+++ post_install.sh
@@ -7,7 +7,8 @@ sysrc -f /etc/rc.conf apache24_enable="YES"
 # Configure the service
 echo "" | sh /usr/local/etc/backuppc/update.sh
 
-sys
+chmod 755 /usr/local/www/cgi-bin/BackupPC_Admi
 
 # Start the service
 service backuppc start 2>/dev/null
+service apache24 restart 2>/dev/null
