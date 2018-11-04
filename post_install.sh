11c1f6117efcd12d7523fe481703f8148f592902
Martin Wilke <miwi@FreeBSD.org>
2018-11-04 21:18:29 +0800

    Typo

--- post_install.sh
+++ post_install.sh
@@ -7,7 +7,7 @@ sysrc -f /etc/rc.conf apache24_enable="YES"
 # Configure the service
 echo "" | sh /usr/local/etc/backuppc/update.sh
 
-chmod 755 /usr/local/www/cgi-bin/BackupPC_Admi
+chmod 755 /usr/local/www/cgi-bin/BackupPC_Admin
 
 # Start the service
 service backuppc start 2>/dev/null
