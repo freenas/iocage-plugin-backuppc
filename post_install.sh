69a6d9ebb39ea56a5ce0c10b870b5b50f6081725
Joel Linn <jl@conductive.de>
2018-12-26 20:58:01 +0100

    Update post_install.sh
    WIP - Modify backuppc configuration and define http default password.

--- post_install.sh
+++ post_install.sh
@@ -6,8 +6,16 @@ sysrc -f /etc/rc.conf apache24_enable="YES"
 
 # Configure the service
 echo "" | sh /usr/local/etc/backuppc/update.sh
+perl -I /usr/local/lib /usr/local/libexec/backuppc/configure.pl \
+  --batch \
+  --config-only \
+  --config-dir /usr/local/etc/backuppc \
+  --config-override CgiImageDirURL="''" \
+  --config-override CgiAdminUsers='backuppc'
 
-chmod 755 /usr/local/www/cgi-bin/BackupPC_Admin
+htpasswd -b -c /usr/local/etc/apache24/htpasswd "backuppc" "password"
+
+chmod 750 /usr/local/www/cgi-bin/BackupPC_Admin
 
 # Start the service
 service backuppc start 2>/dev/null
