d01e35d24a34185eb6c3d6bd64c4c60265b3fa52
Joel Linn <jl@conductive.de>
2018-12-26 21:51:22 +0100

    SSH key generation

--- post_install.sh
+++ post_install.sh
@@ -18,6 +18,13 @@ htpasswd -b -c /usr/local/etc/backuppc/htpasswd "backuppc" "password"
 chmod 750 /usr/local/www/cgi-bin/BackupPC_Admin
 chown backuppc:backuppc -R /usr/local/etc/backuppc
 
+# Set home directory (for ssh keys)
+pw usermod -n backuppc -m -d /home/backuppc
+# Generate ssh keys
+su -m backuppc -c "ssh-keygen -t rsa -N '' -f /home/backuppc/.ssh/id_rsa"
+echo "SSH public key:"
+cat /usr/local/etc/backuppc/.ssh/id_rsa.pub
+
 # Start the service
 service backuppc start 2>/dev/null
 service apache24 restart 2>/dev/null
