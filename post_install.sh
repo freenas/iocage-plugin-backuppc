5d9ab640305654c09eec1117157cd52022817f82
Joel Linn <jl@conductive.de>
2019-02-04 12:50:16 +0100

    Self signed certificate

--- post_install.sh
+++ post_install.sh
@@ -16,14 +16,33 @@ perl -I /usr/local/lib /usr/local/libexec/backuppc/configure.pl \
   --config-only \
   --config-dir /usr/local/etc/backuppc \
   --config-override CgiImageDirURL=\"\" \
-  --config-override CgiAdminUsers=\"${BPC_USER}\" \
+  --config-override CgiAdminUsers=\"backuppc\" \
   --config-override RsyncClientPath=\"/usr/bin/rsync\" \
   >> $LOGFILE
 
+// Create web admin user
 htpasswd -b -c /usr/local/etc/backuppc/htpasswd "backuppc" "password" >> $LOGFILE
 
 chmod 750 /usr/local/www/cgi-bin/BackupPC_Admin
 chown ${BPC_USER}:${BPC_USER} -R /usr/local/etc/backuppc
+
+# Create self signed web certificate
+TLS_DIR=/usr/local/etc/apache24/tls
+mkdir -p ${TLS_DIR}/self-signed
+# not supported by old openssl version:	-addext "subjectAltName = DNS:`hostname`" \
+openssl req -newkey rsa:4096 -nodes \
+  -subj "/O=BackupPC/CN=`hostname`" \
+  -keyout ${TLS_DIR}/self-signed/key.pem \
+  -out ${TLS_DIR}/self-signed/csr.pem \
+  >> $LOGFILE
+openssl x509 \
+  -signkey ${TLS_DIR}/self-signed/key.pem \
+  -in ${TLS_DIR}/self-signed/csr.pem \
+  -req -days 2555 -out ${TLS_DIR}/self-signed/cert.pem \
+  >> $LOGFILE
+TLS_SS_FP=`openssl x509 -in ${TLS_DIR}/self-signed/cert.pem -noout -sha256 -fingerprint`
+chmod -R 600 ${TLS_DIR}
+chown -R ${BPC_USER}:${BPC_USER} ${TLS_DIR}
 
 # Set home directory (for ssh keys)
 BPC_HOME=/home/${BPC_USER}
@@ -46,6 +65,10 @@ echo
 echo "SSH public key:"
 cat ${BPC_HOME}/.ssh/id_rsa.pub
 echo "Add this to the authorized_keys of the client machines you want to backup using ssh public key authentication"
+
+echo
+echo "Fingerprint of the web certificate"
+echo ${TLS_SS_FP}
 
 echo
 echo "Standard username and password is"
